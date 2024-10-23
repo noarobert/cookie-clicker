28cf830d47cdda33bdb55a6959a357de3c794b01
noarobert <noarobertpro@gmail.com>
2024-10-23 15:50:41 +0200

    Colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
 
 export default function Home() {
+
+  const [cookies, setCookies]= useState<number>(0)
+
   return (
-    <div>
-      <p>Hello !</p>
+    <div className="h-screen w-screen flex">
+        <div className="left w-1/4 bg-blue-500"></div>
+        <div className="center flex-1 bg-pink-500"></div>
+        <div className="right w-1/4 bg-yellow-500"></div>
     </div>
   );
 }
